de711f59a1b2e889b39288aade03a2614fbf5f0e
hiba irsheid <hibairsheid@MacBook-Pro-139.local>
2024-03-12 22:50:17 +0200

    adding indication of no internet connection on sign in and sign up screens)

--- lib/homePages/googleSignInHandler.dart
+++ lib/homePages/googleSignInHandler.dart
@@ -4,64 +4,92 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:petsitter/signUp/signUpPage.dart';
 import '../generalAppView.dart';
+import '../utils/connectivityUtil.dart';
 
 class GoogleSignInHandler {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<bool> handleGoogleSignIn(BuildContext context) async {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
-    final GoogleSignIn _googleSignIn = GoogleSignIn();
-    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    bool isConnected =
+        await ConnectivityUtil.checkConnectivityForGoogleAuth(context);
+    if (isConnected) {
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+      final GoogleSignIn _googleSignIn = GoogleSignIn();
+      final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-    try {
-      // Show circular loading indicator while sign-in is in progress
-      showDialog(
-        context: context,
-        barrierDismissible: false, // Prevent users from dismissing the dialog
-        builder: (BuildContext context) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      );
+      try {
+        // Show circular loading indicator while sign-in is in progress
+        showDialog(
+          context: context,
+          barrierDismissible: false, // Prevent users from dismissing the dialog
+          builder: (BuildContext context) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        );
 
-      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      final GoogleSignInAuthentication googleAuth =
-          await googleUser!.authentication;
+        final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser!.authentication;
 
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
 
-          UserCredential authResult =
-        await _auth.signInWithCredential(credential);
+        UserCredential authResult =
+            await _auth.signInWithCredential(credential);
 
-    User? user = authResult.user;
+        User? user = authResult.user;
 
-      final DocumentSnapshot doc =
-          await _firestore.collection('users').doc(googleUser.email).get();
-      if (!doc.exists) {
+        final DocumentSnapshot doc =
+            await _firestore.collection('users').doc(googleUser.email).get();
+        if (!doc.exists) {
+          _googleSignIn.signOut();
+          Navigator.pop(context); // Dismiss loading indicator dialog
+          showSignUpRequiredDialog(
+              context, 'You need to sign up before signing in with Google.');
+        } else {
+          Navigator.pop(context); // Dismiss loading indicator dialog
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => GeneralAppPage()));
+          return true;
+        }
+      } catch (error) {
         _googleSignIn.signOut();
+        print("Got error: ${error.toString()}");
         Navigator.pop(context); // Dismiss loading indicator dialog
-        showSignUpDialog(
-            context, 'You need to sign up before signing in with Google.');
-      } else {
-        Navigator.pop(context); // Dismiss loading indicator dialog
-        Navigator.push(context, MaterialPageRoute(builder: (context) => GeneralAppPage()));
-        return true;
+        showSignUpDialog(context, 'Got error while signing in with Google');
       }
-    } catch (error) {
-      _googleSignIn.signOut();
-      print("Got error: ${error.toString()}");
-      Navigator.pop(context); // Dismiss loading indicator dialog
-      showSignUpDialog(context, 'Got error while signing in with Google');
+      return false;
+    } else {
+      showSignUpDialog(context, 'No internet connection. Unable to authenticate with Google.');
+      return false;
     }
-    return false;
-    
-    }
+  }
 
   void showSignUpDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Sign In Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showSignUpRequiredDialog(BuildContext context, String message) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
